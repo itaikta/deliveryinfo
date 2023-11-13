f07498ed849d1775d609e79b826e9b6d8ca24787
Itai Ktalav <itaiktalav@outlook.com>
2023-11-13 18:12:37 +0200

    populated supplies section from the url (seperated by underscores)

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const urlParams = new URLSearchParams(window.location.search);
-for (const entry of urlParams.entries()) console.log(entry);
+//for (const entry of urlParams.entries()) console.log(entry);
 
 for (const fieldName of ["name", "number", "email", "city", "postCode", "destType", "address", "info"]) {
     const valueName = urlParams.get(fieldName);
     if (valueName) document.getElementById(fieldName).innerHTML = valueName;
+}
+
+if (urlParams.has("supplies")){
+    const supplyList = urlParams.get("supplies").split("_");
+    let suppliesString = "";
+
+    for (const supply of supplyList) {
+        suppliesString += `<li>${supply}</li>`;
+    }
+
+    document.getElementById("supplies").innerHTML = suppliesString;
 }

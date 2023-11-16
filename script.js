83d2ee88f6aa2f0a8e54121784f987470c4dee3c
Itai Ktalav <itaiktalav@outlook.com>
2023-11-16 17:12:11 +0200

    More dynamic supplies listing

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ const urlParams = new URLSearchParams(window.location.search);
 
 for (const fieldName of ["name", "number", "email", "city", "postCode", "destType", "address", "info"]) {
     const valueName = urlParams.get(fieldName);
-    if (valueName) document.getElementById(fieldName).innerHTML = fieldName.toLowerCase() != "info" ? `<b>${valueName}</b>` : `${valueName}`;
+    if (valueName) {
+        document.getElementById(fieldName).innerHTML = fieldName != "info" 
+            ? `<b>${valueName}</b>` 
+            : valueName;
+    }
 }
 
 if (urlParams.has("supplies")) {
-    const supplyList = urlParams.get("supplies").split("_");
+    const supplyStr = urlParams.get("supplies");
+    let supplyList = [];
+
+    // Does't really matter if it contains a mix of these, this order is the priority
+    if (supplyStr.includes("_")){
+        supplyList = supplyStr.split("_");
+    }
+    else if (supplyStr.includes("\n")) {
+        supplyList = supplyStr.includes("\n");
+    }
+    else {
+        supplyList = supplyStr.split(",");
+    }
     
+    console.log(supplyList);
+
     let suppliesString = "";
     for (const supply of supplyList) {
         if (supply) suppliesString += `<li>${supply}</li>\n`;
     }
+
     document.getElementById("supplies").innerHTML = suppliesString;
 }
